49e0a9fdb57e8c3a7b464b67ac1eae0098a70e67
Mohammad W. <38215725+mwandawi99@users.noreply.github.com>
2025-04-13 12:52:02 -0400

    Added logo, sidebar, links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.scss';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
    <>
    <Routes>
       <Route path="/" element={<Layout />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
